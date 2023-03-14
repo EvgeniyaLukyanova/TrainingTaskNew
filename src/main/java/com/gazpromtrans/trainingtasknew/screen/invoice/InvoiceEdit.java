bdfa8c2f48bff0a5fa86b79cdd710d8483c6aff4
EvgeniyaLukyanova <116807300+EvgeniyaLukyanova@users.noreply.github.com>
2023-03-14 10:35:14 +0300

    Add files via upload

--- src/main/java/com/gazpromtrans/trainingtasknew/screen/invoice/InvoiceEdit.java
+++ src/main/java/com/gazpromtrans/trainingtasknew/screen/invoice/InvoiceEdit.java
@@ -1,15 +1,29 @@
 package com.gazpromtrans.trainingtasknew.screen.invoice;
 
+import io.jmix.reportsui.action.list.EditorPrintFormAction;
+import io.jmix.ui.Actions;
+import io.jmix.ui.action.Action;
+import io.jmix.ui.component.Button;
+import io.jmix.ui.component.Component;
+import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import com.gazpromtrans.trainingtasknew.entity.Invoice;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Invoice.edit")
 @UiDescriptor("invoice-edit.xml")
 @EditedEntityContainer("invoiceDc")
 public class InvoiceEdit extends StandardEditor<Invoice> {
+    @Autowired
+    private Button print;
+    @Autowired
+    private Actions actions;
+
     @Subscribe
     public void onInit(InitEvent event) {
-
+        EditorPrintFormAction action = actions.create(EditorPrintFormAction.class);
+        action.setEditor(this);
+        action.setReportOutputName(null);
+        print.setAction(action);
     }
-
 }
