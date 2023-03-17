0d08f3d6937b2e1ed9facf5fc9a525fcdef101b8
EvgeniyaLukyanova <116807300+EvgeniyaLukyanova@users.noreply.github.com>
2023-03-17 14:55:03 +0300

    Add files via upload

--- src/main/java/com/gazpromtrans/trainingtasknew/listener/TaskCompletedEmailSender.java
+++ src/main/java/com/gazpromtrans/trainingtasknew/listener/TaskCompletedEmailSender.java
@@ -1,7 +1,13 @@
 package com.gazpromtrans.trainingtasknew.listener;
 
+import com.gazpromtrans.trainingtasknew.entity.Contract;
 import com.gazpromtrans.trainingtasknew.entity.User;
 import io.jmix.bpm.engine.events.UserTaskCompletedEvent;
+import io.jmix.core.DataManager;
+import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.email.EmailInfo;
+import io.jmix.email.EmailInfoBuilder;
+import io.jmix.email.Emailer;
 import org.flowable.engine.RuntimeService;
 import org.flowable.task.service.delegate.DelegateTask;
 import org.slf4j.Logger;
@@ -11,6 +17,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.flowable.engine.delegate.TaskListener;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Component
@@ -19,6 +26,17 @@ public class TaskCompletedEmailSender implements TaskListener {
 
     @Autowired
     private RuntimeService runtimeService;
+    @Autowired
+    private DataManager dataManager;
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+    @Autowired
+    private Emailer emailer;
+
+    @PostConstruct
+    public void onInit() {
+        log.info("");
+    }
 
 
     @EventListener
@@ -28,7 +46,24 @@ public class TaskCompletedEmailSender implements TaskListener {
 
     @Override
     public void notify(DelegateTask delegateTask) {
-        List<User> userList = (List<User>) runtimeService.getVariable(delegateTask.getExecutionId(), "userList");
-        log.info(userList.toString());
+        //List<User> userList = (List<User>) runtimeService.getVariable(delegateTask.getExecutionId(), "userList");
+        //delegateTask.getVariables();
+
+        Contract contract = (Contract) delegateTask.getVariable("contract");
+        //log.info(userList.toString());
+        User user = dataManager.load(User.class)
+                .query("select u from User u where u.username = :username")
+                .parameter("username", currentAuthentication.getUser().getUsername())
+                .one();
+        String emailTitle = "Смена статуса";
+        String emailBody = "По договору №" + contract.getNumber() + "\n" +
+                "был изменен статус на " + contract.getState().getName();
+        EmailInfo emailInfo = EmailInfoBuilder.create()
+                .setAddresses(user.getEmail())
+                .setSubject(emailTitle)
+                .setFrom(null)
+                .setBody(emailBody)
+                .build();
+        emailer.sendEmailAsync(emailInfo);
     }
 }
